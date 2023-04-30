bb29926b86f5c67190b95e101251f5d739ec263a
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2023-04-30 18:58:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def gif(path,name):
     images=[]
     for img in image_path:
         images.append(imageio.imread(img))
-    imageio.mimwrite(f'CLT/{name}.gif',images,fps=10)
+    imageio.mimwrite(f'{path}/{name}.gif',images,fps=5)
 
 def normal(x,mu,sigma):
     return 1/np.sqrt(2*np.pi*sigma**2)*np.exp(-((x-mu)/sigma)**2)
@@ -82,71 +82,71 @@ q3=moneyspent[int(3*count/4)]
 mean=np.mean(moneyspent)
 print(f' count = {count}\n mean = {mean}\n std = {std}\n max = {max}\n min = {min}\n 25% = {q1}\n 50% = {q2}\n 75% = {q3}')
 
-#Histograms
-counts,edges,bars=plt.hist(physicalactivity,bins=range(0,51,2))
-plt.xlabel('Physical activity per week in hours')
-plt.ylabel('Count')
-plt.bar_label(bars)
-plt.grid()
-plt.show()
-counts,edges,bars=plt.hist(sleeptime,bins=range(0,14))
-plt.bar_label(bars)
-plt.xlabel('Sleep time in hours per day')
-plt.ylabel('Count')
-plt.grid()
-plt.show()
-counts,edges,bars=plt.hist(eatoutfrequency,bins=range(0,91,3))
-plt.bar_label(bars)
-plt.xlabel('Number of times a student eats outside per month')
-plt.ylabel('Count')
-plt.grid()
-plt.show()
-counts,edges,bars=plt.hist(moneyspent,bins=range(0,500,40))
-plt.bar_label(bars)
-plt.xlabel('Money spent on food per day')
-plt.ylabel('Count')
-plt.grid()
-plt.show()
+# #Histograms
+# counts,edges,bars=plt.hist(physicalactivity,bins=range(0,51,2))
+# plt.xlabel('Physical activity per week in hours')
+# plt.ylabel('Count')
+# plt.bar_label(bars)
+# plt.grid()
+# plt.show()
+# counts,edges,bars=plt.hist(sleeptime,bins=range(0,14))
+# plt.bar_label(bars)
+# plt.xlabel('Sleep time in hours per day')
+# plt.ylabel('Count')
+# plt.grid()
+# plt.show()
+# counts,edges,bars=plt.hist(eatoutfrequency,bins=range(0,91,3))
+# plt.bar_label(bars)
+# plt.xlabel('Number of times a student eats outside per month')
+# plt.ylabel('Count')
+# plt.grid()
+# plt.show()
+# counts,edges,bars=plt.hist(moneyspent,bins=range(0,500,40))
+# plt.bar_label(bars)
+# plt.xlabel('Money spent on food per day')
+# plt.ylabel('Count')
+# plt.grid()
+# plt.show()
 
-#Boxplots
-veg=[]
-Nonveg=[]
-Egg=[]
-for i in range(data.shape[0]):
-    if(data[i][0]=='Non-vegetarian'):
-        Nonveg.append(data[i])
-    elif(data[i][0]=='Vegetarian'):
-        veg.append(data[i])
-    else:
-        Egg.append(data[i])
-columns=['Physical activity per week in hours','Sleep time in hours per day','Number of times a student eats outside per month','Money spent on food per day']
-veg=np.array(veg) 
-Nonveg=np.array(Nonveg) 
-Egg=np.array(Egg)  
-colors=['green','red','brown']
-for i in range(1,5):
-    temp=[veg[:,i],Nonveg[:,i],Egg[:,i]]
-    # Create the boxplot
-    fig, ax = plt.subplots()
-    box = ax.boxplot(temp, patch_artist=True, vert=True)
-    # Set the xticks and labels
-    xticks = [1, 2, 3]
-    xticklabels = ['Veg', 'Non-veg', 'Veg+Egg']
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(xticklabels)
-    ax.set_ylabel(columns[i-1])
-    # Set the facecolor of each box
-    for i, patch in enumerate(box['boxes']):
-        patch.set_facecolor(colors[i])
-    plt.grid()
-    plt.show()
+# #Boxplots
+# veg=[]
+# Nonveg=[]
+# Egg=[]
+# for i in range(data.shape[0]):
+#     if(data[i][0]=='Non-vegetarian'):
+#         Nonveg.append(data[i])
+#     elif(data[i][0]=='Vegetarian'):
+#         veg.append(data[i])
+#     else:
+#         Egg.append(data[i])
+# columns=['Physical activity per week in hours','Sleep time in hours per day','Number of times a student eats outside per month','Money spent on food per day']
+# veg=np.array(veg) 
+# Nonveg=np.array(Nonveg) 
+# Egg=np.array(Egg)  
+# colors=['green','red','brown']
+# for i in range(1,5):
+#     temp=[veg[:,i],Nonveg[:,i],Egg[:,i]]
+#     # Create the boxplot
+#     fig, ax = plt.subplots()
+#     box = ax.boxplot(temp, patch_artist=True, vert=True)
+#     # Set the xticks and labels
+#     xticks = [1, 2, 3]
+#     xticklabels = ['Veg', 'Non-veg', 'Veg+Egg']
+#     ax.set_xticks(xticks)
+#     ax.set_xticklabels(xticklabels)
+#     ax.set_ylabel(columns[i-1])
+#     # Set the facecolor of each box
+#     for i, patch in enumerate(box['boxes']):
+#         patch.set_facecolor(colors[i])
+#     plt.grid()
+#     plt.show()
 
 
 #CLT
-pairs = []
-mu=[]
 ks=range(40,70)
 for k in ks:
+    pairs = []
+    mu=[]   
     for i in range(1,1000):
         sleeptime1=list(sleeptime)
         for p in range(len(sleeptime) // k):
@@ -156,16 +156,27 @@ for k in ks:
             mu.append(sum/k)
     # print(len(pairs))
     mu.sort()
-    unique_mu=list(set(mu))
-    # print(unique_mu)
-    freq=[]
-    for i in range(len(unique_mu)):
-        freq.append(mu.count(unique_mu[i])/len(mu))
+#This part of code shows histogram rep for CLT
     fig1, ax1= plt.subplots()
-    ax1.grid()
+    counts,edges,bars=ax1.hist(mu,bins=np.linspace(1,13,100),label=f'n={k}')
+    plt.xlim(right=4,left=8)
+    plt.grid()
+    plt.legend()
     plt.xlim(left=6,right=8)
-    plt.ylim(top=0.05)
-    ax1.scatter(unique_mu,freq,label=f'n={k}')
-    ax1.legend()
-    plt.savefig(f'CLT\{k:003}',dpi=100,facecolor='white')
-gif('CLT','CLT')
+    plt.ylim(top=1200)
+    plt.savefig(f'CLT_histogram\{k:003}',dpi=100,facecolor='white')
+gif('CLT_histogram','CLT_histogram')
+#This part of code show points for CLT
+#     unique_mu=list(set(mu))
+#     # print(unique_mu)
+#     freq=[]
+#     for i in range(len(unique_mu)):
+#         freq.append(mu.count(unique_mu[i])/len(mu))
+#     fig1, ax1= plt.subplots()
+#     ax1.grid()
+#     plt.xlim(left=6,right=8)
+#     plt.ylim(top=0.05)
+#     ax1.scatter(unique_mu,freq,label=f'n={k}')
+#     ax1.legend()
+#     plt.savefig(f'CLT\{k:003}',dpi=100,facecolor='white')
+# gif('CLT','CLT')
